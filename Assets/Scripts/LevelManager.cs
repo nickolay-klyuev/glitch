95d6d8e659697dd4c892e255cb88771d2bf1cf67
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-02-20 13:59:05 +0300

    add project

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	public float autoLoadNextLevelAfter;
+
+	void Start()
+	{	
+		if (autoLoadNextLevelAfter == 0)
+		{
+			Debug.Log("Level auto load disabled");
+		}
+		else 
+		{
+			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+    public void LoadLevel(string name)
+	{
+		Debug.Log ("New Level load: " + name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest()
+	{
+		Debug.Log ("Quit requested");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.sceneCount);
+	}
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] levelMusicChangeArray;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisMusic = levelMusicChangeArray[level];
+        Debug.Log("Loaded music: " + thisMusic);
+        if (thisMusic)
+        {
+            audioSource.clip = thisMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
