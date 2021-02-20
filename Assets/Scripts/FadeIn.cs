2be0784c030069402584967ba8fb163783d2f043
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-02-20 14:27:28 +0300

    add fade in effect for start scene

--- Assets/Scripts/FadeIn.cs
+++ Assets/Scripts/FadeIn.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeIn : MonoBehaviour
 {
-    public float 
+    public float fadeInTime;
+    private Image fadePanel;
+    private Color currentColor = Color.black;
     void Start()
     {
-        
+        fadePanel = GetComponent<Image>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Time.timeSinceLevelLoad < fadeInTime)
+        {
+            float alphaChange = Time.deltaTime / fadeInTime;
+            currentColor.a -= alphaChange;
+            fadePanel.color = currentColor;
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
