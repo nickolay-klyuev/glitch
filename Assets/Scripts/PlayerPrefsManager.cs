3207b5682f35b04458824c7e3a8610aa4357c65e
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-02-21 13:31:57 +0300

    add PlayerPrefsManager.cs

--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -1,18 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerPrefsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    const string MASTER_VOLUME_KEY = "master_volume";
+    const string DIFFICULTY_KEY = "difficulty";
+    const string LEVEL_KEY = "level_unlocked_";
+
+    public static void SetMasterVolume(float volume)
     {
-        
+        if (volume > 0f && volume < 1f)
+        {
+            PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, volume);
+        }
+        else 
+        {
+            Debug.LogError("Master volume is out of range");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static float GetMasterVolume(float volume)
     {
-        
+        return PlayerPrefs.GetFloat(MASTER_VOLUME_KEY);
+    }
+
+    public static void UnlockLevel(int level)
+    {
+        if (level <= SceneManager.sceneCountInBuildSettings - 1)
+        {
+            PlayerPrefs.SetInt(LEVEL_KEY + level.ToString(), 1); // Use 1 for true
+        }
+        else
+        {
+            Debug.LogError("Trying to unlock level not in build order");
+        }
+    }
+
+    public static bool IsLevelUnlocked(int level)
+    {
+        if (level <= SceneManager.sceneCountInBuildSettings - 1)
+        {
+            int levelUnlocked = PlayerPrefs.GetInt(LEVEL_KEY + level.ToString());
+            if (levelUnlocked == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            Debug.LogError("Trying to get info for non existing level");
+            return false;
+        }
     }
 }
