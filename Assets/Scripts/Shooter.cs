309180438af6a99584495a372bed0e7b171b84b4
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-03-01 14:11:51 +0300

    create shooters system

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,6 +6,8 @@ public class Shooter : MonoBehaviour
 {
     public GameObject projectile, gun;
     private GameObject projectileParent;
+    private Animator animator;
+    private Spawner laneSpawner;
     
     void Start()
     {
@@ -14,6 +16,56 @@ public class Shooter : MonoBehaviour
         {
             projectileParent = new GameObject("Projectiles");
         }
+        animator = gameObject.GetComponent<Animator>();
+
+        SetLaneSpawner();
+    }
+
+    void Update()
+    {
+        if (IsAttackerAheadInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    bool IsAttackerAheadInLane()
+    {
+        int attackersCount = laneSpawner.transform.childCount;
+        if (attackersCount <= 0)
+        {
+            return false;
+        }
+        
+        foreach(Transform child in laneSpawner.transform)
+        {
+            if (child.transform.position.x >= gameObject.transform.position.x)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void SetLaneSpawner()
+    {
+        Spawner[] spawnersArray = GameObject.FindObjectsOfType<Spawner>();
+
+        foreach(Spawner spawner in spawnersArray)
+        {
+            if (spawner.transform.position.y == gameObject.transform.position.y)
+            {
+                laneSpawner = spawner;
+                return;
+            }
+        }
+
+        Debug.LogError(name + " can't find spawner");
     }
 
     private void Fire()
