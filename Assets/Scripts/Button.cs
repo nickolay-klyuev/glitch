d85d9e519d8d9cc02a963986aa33d21f8397deda
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-03-03 12:40:55 +0300

    add cost text

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour
 {
     public GameObject defenderPrefab;
     public static GameObject selectedDefender;
     private Button[] buttonArray;
+    private Text costText;
+
     // Start is called before the first frame update
     void Start()
     {
         buttonArray = GameObject.FindObjectsOfType<Button>();
+        costText = gameObject.GetComponentInChildren<Text>();
+        if (costText == null)
+        {
+            Debug.LogWarning(name + ": missing cost text element");
+        }
+        else
+        {
+            costText.text = defenderPrefab.GetComponent<Defender>().starsCost.ToString();
+        }
     }
 
     // Update is called once per frame
